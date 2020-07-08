067c6741134b8e86e3d48b18efc2db127bcfb261
ryawr <rahulyadavawrraj@gmail.com>
2020-07-09 01:31:10 +0530

    Update Object_detection_image.py

--- Object_detection_image.py
+++ Object_detection_image.py
@@ -1,11 +1,4 @@
-######## Image Object Detection Using Tensorflow-trained Classifier #########
-#
-# Author: Evan Juras
-# Date: 1/15/18
-# Description: 
-# This program uses a TensorFlow-trained neural network to perform object detection.
-# It loads the classifier and uses it to perform object detection on an image.
-# It draws boxes, scores, and labels around the objects of interest in the image.
+# Draws boxes, scores, and labels around the objects.
 
 ## Some of the code is copied from Google's example at
 ## https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
@@ -13,9 +6,8 @@
 ## and some is copied from Dat Tran's example at
 ## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
 
-## but changed it to make it more understandable to me.
+## but changed it for custom use.
 
-# Import packages
 import os
 import cv2
 import numpy as np
@@ -27,7 +19,6 @@ import io
 # This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
 
-# Import utilites
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
@@ -39,7 +30,6 @@ FLAGS = flags.FLAGS
 MODEL_NAME = 'inference_graph'
 IMAGE_NAME = FLAGS.IMAGE_NAME
 
-# Grab path to current working directory
 CWD_PATH = os.getcwd()
 
 # Path to frozen detection graph .pb file, which contains the model that is used
@@ -56,10 +46,6 @@ PATH_TO_IMAGE = os.path.join(CWD_PATH,IMAGE_NAME)
 NUM_CLASSES = 6
 
 # Load the label map.
-# Label maps map indices to category names, so that when our convolution
-# network predicts `5`, we know that this corresponds to `king`.
-# Here we use internal utility functions, but anything that returns a
-# dictionary mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
@@ -81,31 +67,25 @@ with detection_graph.as_default():
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
 # Output tensors are the detection boxes, scores, and classes
-# Each box represents a part of the image where a particular object was detected
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
-# Each score represents level of confidence for each of the objects.
-# The score is shown on the result image, together with the class label.
 detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
 # Number of objects detected
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-# Load image using OpenCV and
 # expand image dimensions to have shape: [1, None, None, 3]
-# i.e. a single-column array, where each item in the column has the pixel RGB value
 image = cv2.imread(PATH_TO_IMAGE)
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image_expanded = np.expand_dims(image_rgb, axis=0)
 
-# Perform the actual detection by running the model with the image as input
+# detection
 (boxes, scores, classes, num) = sess.run(
     [detection_boxes, detection_scores, detection_classes, num_detections],
     feed_dict={image_tensor: image_expanded})
 
-# Draw the results of the detection (aka 'visulaize the results')
-
+# visulaizing the results
 vis_util.visualize_boxes_and_labels_on_image_array(
     image,
     np.squeeze(boxes),
