3f06cab1b4f24a94bbddcb3ca081a76172fa5248
ryawr <rahulyadavawrraj@gmail.com>
2020-07-09 01:09:09 +0530

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -1,10 +1,10 @@
 """
 Usage:
   # From tensorflow/models/
-  # Create train data:
+  # train data:
   python generate_tfrecord.py --csv_input=images/train_labels.csv --image_dir=images/train --output_path=train.record
 
-  # Create test data:
+  # test data:
   python generate_tfrecord.py --csv_input=images/test_labels.csv  --image_dir=images/test --output_path=test.record
 """
 from __future__ import division
